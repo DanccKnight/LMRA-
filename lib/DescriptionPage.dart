dc3007679d5cb0547b833d2e6b2786b839fa1a1b
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-24 01:02:45 +0530

    Added an image as the background image of the sliver app bar

--- lib/DescriptionPage.dart
+++ lib/DescriptionPage.dart
@@ -9,23 +9,35 @@ class _DescriptionPageState extends State<DescriptionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          shape: ContinuousRectangleBorder(
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30.0),bottomRight: Radius.circular(30.0))
-          ),
-          elevation: 5,
-          expandedHeight: 200,
-          backgroundColor: Colors.blueAccent,
-          pinned: true,
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text('My Hero Academia'),
+        body: CustomScrollView(slivers: <Widget>[
+      SliverAppBar(
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back),
+          onTap: () => Navigator.of(context).pop(),
+        ),
+        shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50.0),
+                bottomRight: Radius.circular(50.0))),
+        elevation: 5,
+        expandedHeight: 200,
+        backgroundColor: Colors.blue,
+        pinned: true,
+        floating: true,
+        flexibleSpace: FlexibleSpaceBar(
+          /*centerTitle: true,
+          title: Text(
+            'Boku no Hero Academia',
+          ),*/
+          background: ClipRRect(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0)),
+            child: Image.network(
+                "https://images4.alphacoders.com/698/thumb-1920-698782.jpg"),
           ),
         ),
-        
-      ],
-    ));
+      ),
+    ]));
   }
 }
