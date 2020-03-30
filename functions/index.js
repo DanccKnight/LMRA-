6c8170c316f20f85a5c261095ad4837b1cba89d3
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-30 23:58:12 +0530

    Got rid of a reference error

--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,11 @@ var msgData;
 var token = "eUwv2hxyzBU:APA91bF-e4bB1IFEv_IJAs67F1tdchvQDKrxikT0krp5kl2VSJ_lN76kwvkn0l3NUKz_r5Ut7q-FcUG7IUE4XGKgJDCmI3czvPEQQkn78IX6W9Xb0o6vyQ0GyLDXN84ZY_PEhaUTgp66";
 
 exports.offerTrigger = functions.firestore.document(
-    'My Hero Academia'/{chapter}
+    'My Hero Academia/{chapter}'
     ).onCreate((snapshot,context) => {
         msgData = snapshot.data();
         var payload = {
-            "notifications": {
+            "notification": {
                 "title": "A new chapter has been released",
                 "body": "Read chapter " + msgData.number + " now!",
                 "sound": "default"
@@ -19,7 +19,7 @@ exports.offerTrigger = functions.firestore.document(
         }
 
         return admin.messaging().sendToDevice(token,payload).then((response) => {
-
+            console.log('Pushed notification');
         }).catch((error) => {
             console.log(error);
         })
