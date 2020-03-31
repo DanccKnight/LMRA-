d329bf30547e3681cbe946c80be859e67fe13cce
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-31 23:52:00 +0530

    Added Signin page

---
+++ lib/UI/SignIn.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lmra/Data/Auth.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Sign in with Google'),
+            onPressed: () async => Auth.signInWithGoogle()
+                .then((value) => Navigator.of(context).pushReplacementNamed('/HomePage'))),
+      ),
+    );
+  }
+}
