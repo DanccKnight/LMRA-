373795f251089e4a75f178989199efe43c9c059e
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-31 23:55:32 +0530

    Added User model and singleton

---
+++ lib/Data/User.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'User.g.dart';
+
+@JsonSerializable()
+class User {
+  String name;
+  String uid;
+  String email;
+  String profileImage;
+  String token;
+
+  User({this.name, this.uid, this.email, this.profileImage, this.token});
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
