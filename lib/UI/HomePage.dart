3916b50dcd73d23a4cea3c097ec5e0c55824a6d8
Ansuman Acharya <nitinansu12@gmail.com>
2020-04-01 01:32:42 +0530

    Added Drawer

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lmra/Data/UserSingleton.dart';
+import 'package:lmra/Data/Auth.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -11,12 +12,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text(UserSingleton().user.name),
+              accountEmail: Text(UserSingleton().user.email),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      UserSingleton().user.profileImage == null
+                          ? CircleAvatar()
+                          : UserSingleton().user.profileImage),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("About"),
+              onTap: () => _showAbout(),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Sign out"),
+              onTap: () {
+                Auth.logoutUser().then((value) =>
+                    Navigator.of(context).pushReplacementNamed('/SignInPage'));
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Column(
@@ -32,5 +63,26 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.of(context).pushNamed('/DescriptionPage')),
     );
+  }
+
+  void _showAbout() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("About The App"),
+            content: RichText(
+                text: TextSpan(
+              text:
+                  "This app has been made by Ansuman Acharya. If you have any reviews/feedback, drop me an email at ansuman91225@gmail.com",
+              style: TextStyle(color: Colors.black),
+            )),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Close'))
+            ],
+          );
+        });
   }
 }
