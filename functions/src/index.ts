814188011801c9cf112234aa4aadf2d2ea0b3747
Ansuman Acharya <nitinansu12@gmail.com>
2020-04-01 01:33:30 +0530

    Updated the function to send notifications to all users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,31 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
-var key = "eUwv2hxyzBU:APA91bF-e4bB1IFEv_IJAs67F1tdchvQDKrxikT0krp5kl2VSJ_lN76kwvkn0l3NUKz_r5Ut7q-FcUG7IUE4XGKgJDCmI3czvPEQQkn78IX6W9Xb0o6vyQ0GyLDXN84ZY_PEhaUTgp66"
+const db = admin.firestore();
+//var key = "eUwv2hxyzBU:APA91bF-e4bB1IFEv_IJAs67F1tdchvQDKrxikT0krp5kl2VSJ_lN76kwvkn0l3NUKz_r5Ut7q-FcUG7IUE4XGKgJDCmI3czvPEQQkn78IX6W9Xb0o6vyQ0GyLDXN84ZY_PEhaUTgp66"
 const fcm = admin.messaging();
 
 export const sendToTheDevice = functions.firestore
     .document('BNHA/{chapterId}')
     .onCreate(async snapshot => {
         const chapter = snapshot.data();
+        var tokens = [];
+
+        await db.collection('Users').get()
+            .then((snapshot) => {
+
+                if(snapshot.empty)
+                    console.log('No devices');
+                else {
+
+                    for(var doc of snapshot.docs){
+                        tokens.push(doc.data().token)
+                    }
+                }
+            })
+
+
+
         const payload = {
             notification: {
                 title: 'A new chapter has been released',
@@ -17,5 +35,5 @@ export const sendToTheDevice = functions.firestore
             },
         };
 
-        return fcm.sendToDevice(key, payload);
+        return fcm.sendToDevice(tokens, payload);
     });
