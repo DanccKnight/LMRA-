d1c1b87350408b2c184af5d1ad167d04e8536eeb
Ansuman Acharya <nitinansu12@gmail.com>
2020-04-13 22:31:34 +0530

    Finished UI for Signin page

--- lib/UI/SignIn.dart
+++ lib/UI/SignIn.dart
@@ -7,14 +7,40 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          child: Text('Sign in with Google'),
-            onPressed: () async => Auth.signInWithGoogle()
-                .then((value) => Navigator.of(context).pushReplacementNamed('/DescriptionPage'))),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: FlutterLogo(size: 150),
+              ),
+              Container(
+                  padding: EdgeInsets.fromLTRB(60.0, 100.0, 60.0, 0.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.blue,
+                    child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width,
+                      onPressed: () async {
+                        await Auth.signInWithGoogle().then((value) {
+                          Navigator.of(context)
+                              .pushReplacementNamed('/DescriptionPage');
+                        });
+                      },
+                      child: Text("Login with Google",
+                          textAlign: TextAlign.center,
+                          style: TextStyle().copyWith(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  )),
+            ]),
       ),
     );
   }
